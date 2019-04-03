013f3305283bedad20468764167a334df5c50eb6
Nitin Patel <nitinpatel278@gmail.com>
2019-04-04 00:46:47 +0530

    make snake array and increase its length

--- src/GameState.hs
+++ src/GameState.hs
@@ -5,25 +5,44 @@ import           Graphics.Gloss.Interface.Pure.Game
 snakeSize = 30 :: Float
 
 data SnakeGame = Game
-  { snakePosition :: (Float, Float)
+  { snakePosition :: [(Float, Float)]
   , direction     :: (Float, Float)
   }
 
-initialState = Game {snakePosition = (-285, 285), direction = (1, 0)}
+initialState =
+  Game
+    { snakePosition = [(-285, 285), (-270, 285), (-255, 285)]
+    , direction = (1, 0)
+    }
 
-renderGame game = translate x y $ rectangleSolid snakeSize snakeSize
+renderGame game = pictures [snakeBody]
   where
-    (x, y) = snakePosition game
+    snakeBody = renderFullSnake (snakePosition game)
+
+renderSnakeBodyPart (x, y) = translate x y $ rectangleSolid snakeSize snakeSize
+
+renderFullSnake snakePosition = pictures $ map renderSnakeBodyPart snakePosition
 
 nextFrame _ game = game {snakePosition = nextSnakePosition}
   where
     nextSnakePosition = moveSnake (direction game) (snakePosition game)
 
-moveSnake :: (Float, Float) -> (Float, Float) -> (Float, Float)
-moveSnake direction currentPosition = nextPosition
+moveSnake direction snakePosition = nextPosition
   where
     (dirX, dirY) = direction
-    (x, y) = currentPosition
-    nextPosition = (x + (dirX * snakeSize), y + (dirY * snakeSize))
+    (headX, headY) = head snakePosition
+    newHead = (headX + (dirX * snakeSize), headY + (dirY * snakeSize))
+    nextPosition = newHead : init snakePosition
 
+changeDirection newDirection game = game {direction = newDirection}
+
+handleKeys (EventKey (SpecialKey KeyUp) _ _ _) game =
+  changeDirection (0, 1) game
+handleKeys (EventKey (SpecialKey KeyDown) _ _ _) game =
+  changeDirection (0, -1) game
+handleKeys (EventKey (SpecialKey KeyLeft) _ _ _) game =
+  changeDirection (-1, 0) game
+handleKeys (EventKey (SpecialKey KeyRight) _ _ _) game =
+  changeDirection (1, 0) game
+handleKeys (EventKey (Char 'r') _ _ _) game = initialState
 handleKeys _ game = game
